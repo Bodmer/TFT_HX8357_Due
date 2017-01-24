0e69fe0b566dc7cdff776b713e81e4ebf783cfad
Bodmer <bodmer@anola.net>
2017-01-24 13:17:12 +0000

    Correct bug spotted by michibures

--- TFT_HX8357_Due.cpp
+++ TFT_HX8357_Due.cpp
@@ -254,15 +254,6 @@ void TFT_HX8357_Due::init(void)
   pinMode(A0, OUTPUT);
   digitalWrite(A0, HIGH);
 
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-
   // toggle RST low to reset
     digitalWrite(_rst, HIGH);
     delay(50);
